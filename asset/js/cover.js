fc7e37b6b1ed8212055a69222a8910ea2590036c
anoer16 <na7278685@gmail.com>
2026-05-16 23:58:12 +0700

    Update cover.js

--- asset/js/cover.js
+++ asset/js/cover.js
@@ -10,7 +10,33 @@
 
     // 3. Tambahkan class 'buka-scroll' ke tag <body> untuk mengaktifkan kembali fungsi scroll halaman
     document.body.classList.add('buka-scroll');
-    
+
+
+    function bukaUndangan() {
+  const coverSection = document.querySelector('.cover-section');
+  const musik = document.getElementById('musikUndangan'); // Ambil elemen musik
+
+  if (coverSection) {
+    coverSection.classList.add('hidden');
+  }
+
+  document.body.classList.add('buka-scroll');
+
+  // MULAI PUTAR MUSIK
+  if (musik) {
+    musik.play();
+  }
+
+  setTimeout(() => {
+    const sectionAyat = document.querySelector('#ayat'); 
+    if (sectionAyat) {
+      sectionAyat.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  }, 500); 
+}
     // --- TIPS AUDIO ---
     // Jika nanti kamu mau menambahkan musik latar otomatis berputar pas gerbang kebuka,
     // kamu tinggal taruh kode pemutar audionya di bawah sini, contoh:
